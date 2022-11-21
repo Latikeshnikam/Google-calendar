29e59e49c8fa07f20d2680783f07bdf4086c76be
Latikeshnikam <latikesh2121@gmail.com>
2022-11-21 11:55:52 +0530

    remove cahed files

--- src/Components/constants.js
+++
@@ -1,3 +0,0 @@
-export const Weeks = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
-export const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November', 'December']
